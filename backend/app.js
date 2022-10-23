fbdb428b1ba622a9d30a68ba659fa066dbb6d40b
Marco Folco <marcofolco28@gmail.com>
2022-10-23 11:31:24 -0300

    Tarea M5U3 Se agrega el modelo para la interaccion con la base de datos Se realiza una query desde la pagina de login.js para comprobar que funciona correctamente Se crean las tablas pertinentes al proyecto (pages y travels)

--- backend/app.js
+++ backend/app.js
@@ -4,9 +4,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
+require('dotenv').config()
 
 var indexRouter = require('./routes/index');
 var loginRouter = require('./routes/admin/login');
+
 
 var app = express();
 
--- backend/routes/admin/login.js
+++ backend/routes/admin/login.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var pool = require('../../models/bd');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  pool.query("SELECT * FROM travels tr INNER JOIN pages pg ON tr.page_fk = pg.id").then(resultados => {
+    console.log(resultados);
+  })
   res.render('admin/login', {
     layout: 'admin/layout'
   });
